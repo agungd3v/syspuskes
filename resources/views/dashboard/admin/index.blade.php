f60c9ece9ca92f54f131075318748e8c71ce6c8c
agungd3v <agungd3v@gmail.com>
2021-11-14 21:32:26 +0700

    .

--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -6,6 +6,7 @@
   <div class="col-md-12">
     <div class="content-panel" style="padding: 10px 20px">
       <h3>Selamat Bekerja</h3>
+      <img src="{{ asset('IMG_20201229_103830.jpg') }}" style="width: 100%">
     </div>
   </div>
 </div>
